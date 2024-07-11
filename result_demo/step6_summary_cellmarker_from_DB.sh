b922720a896360b09b53a26583b03cd7915e4ea3
chengpeng1116 <166687326+chengpeng1116@users.noreply.github.com>
2024-07-11 09:22:05 +0800

    Update step6_summary_cellmarker_from_DB.sh

--- result_demo/step6_summary_cellmarker_from_DB.sh
+++ result_demo/step6_summary_cellmarker_from_DB.sh
@@ -1,3 +1,3 @@
 #Parse the PDF of the literature under the analysis path into txt format # Extract all the literature under the analysis path using cell markers
-head -n 1 ~/result_demo//paper_process_result/26940531/cellmarker_result.csv > ~/result_demo/cellmarker_result_all.csv
-cat ~/result_demo/paper_process_result/*/cellmarker_result.csv | grep -v "PMID"  >> ~/result_demo/cellmarker_result_all.csv
+head -n 1 ~./result_demo//paper_process_result/26940531/cellmarker_result.csv > ~./result_demo/cellmarker_result_all.csv
+cat ~./result_demo/paper_process_result/*/cellmarker_result.csv | grep -v "PMID"  >> ~./result_demo/cellmarker_result_all.csv
