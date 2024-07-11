92c4afcfbce0ce4cd7420744dd761c8a307ac837
chengpeng1116 <166687326+chengpeng1116@users.noreply.github.com>
2024-07-11 09:09:51 +0800

    Update step4_pdf2txt_a04.sh

--- result_demo/step4_pdf2txt_a04.sh
+++ result_demo/step4_pdf2txt_a04.sh
@@ -1,2 +1,2 @@
-#将分析路径下的文献PDF解析为txt格式
-/mnt/icfs/personal/xiaolingzhang/miniconda3/envs/grobid/bin/python /mnt/rorke/personal/pengcheng/project_collect/Cellmarker_NLP/pipeline_test/pipeline_sh/pdf2txt.py --Analysis_path /mnt/rorke/personal/pengcheng/project_collect/Cellmarker_NLP/pipeline_test//paper_process_result
+#Parse the PDF of the literature under the analysis path into txt format
+python ~./script/pdf2txt.py --Analysis_path ~./result_demo/paper_process_result
