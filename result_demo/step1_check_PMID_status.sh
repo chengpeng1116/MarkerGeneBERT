ac5a1a7431f037cf2188b8c991380ad6f24131bf
chengpeng1116 <166687326+chengpeng1116@users.noreply.github.com>
2024-07-11 08:55:20 +0800

    Update step1_check_PMID_status.sh

--- result_demo/step1_check_PMID_status.sh
+++ result_demo/step1_check_PMID_status.sh
@@ -1,2 +1,2 @@
-# 用于检测分析路径下各文献分析状态，并将分析状态结果保存至DB_PMID_status.csv
-/mnt/icfs/work/singlecelldevelopment/miniconda3/envs/Cellmarker_NLP/bin/python /mnt/rorke/personal/pengcheng/project_collect/Cellmarker_NLP/pipeline_test/pipeline_sh/check_PMID_status.py  --check_path /mnt/rorke/personal/pengcheng/project_collect/Cellmarker_NLP/pipeline_test//paper_process_result --output_path /mnt/rorke/personal/pengcheng/project_collect/Cellmarker_NLP/pipeline_test//DB_PMID_status.csv
+# Used to detect the analysis status of various literature under the analysis path, and save the analysis status results to DB-PMID_status.csv
+python ~./script/check_PMID_status.py  --check_path ~./result_demo/paper_process_result --output_path ~./result_demo/DB_PMID_status.csv
